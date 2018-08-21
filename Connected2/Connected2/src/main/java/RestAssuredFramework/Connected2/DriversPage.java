23976cd505c4f58600ea071bc6258f2fc839e767
ucreatepriyanka <priyankadogra@ucreate.co.in>
2018-08-21 09:51:08 +0530

    driverlogin

---
+++ Connected2/Connected2/src/main/java/RestAssuredFramework/Connected2/DriversPage.java
@@ -0,0 +1,17 @@
+package RestAssuredFramework.Connected2;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+import io.restassured.specification.RequestSpecification;
+import static io.restassured.RestAssured.*;
+
+public class DriversPage {
+	
+	
+	public void ApplyForAJobLicenseDates() {
+		
+		
+		
+	}
+
+}
